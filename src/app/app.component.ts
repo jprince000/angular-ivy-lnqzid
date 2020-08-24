83a563266fb0b0529ec80e48f307b9c471d25ec0
jprince000 <jprince000@gmail.com>
2020-08-24 17:40:20 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import { pipe, from } from "rxjs";
+import { concatMap, filter, finalize, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,27 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  makeItHappen() {
+    from([1,2,3])
+    .pipe(
+      this.myPipe(),
+      map(val => val * 2)
+    )
+    .subscribe(val => {
+      console.log(val);
+    })
+  }
+
+  dateMe() {
+    console.log(new Date('booger').toISOString());
+  }
+
+
+  myPipe = () => pipe(
+    map((val: number) => val * 2),
+    map((val: number) => val * 2),
+    map((val: number) => val * 2),
+  ); 
+
 }
